0077394042f6b8e4c2dad36ec90c8cbda783dd3d
Your Name <you@example.com>
2019-03-17 18:03:43 +0800

    2019 / 3 / 17

--- practice03/ViewController.swift
+++ practice03/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var catImage: UIImageView!
+    
+    let catImgArr = ["cat1", "cat2", "cat3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func changeCatSegment(_ sender: UISegmentedControl) {
+//        if sender.selectedSegmentIndex == 0 {
+//            catImage.image = UIImage(named: "cat1")
+//        } else if sender.selectedSegmentIndex == 1 {
+//            catImage.image = UIImage(named: "cat2")
+//        } else if sender.selectedSegmentIndex == 2 {
+//            catImage.image = UIImage(named: "cat3")
+//        }
+        catImage.image = UIImage(named: catImgArr[sender.selectedSegmentIndex])
+    }
 }
 
